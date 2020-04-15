82eeb01391d684bbd4ff42af8351a1ea81e83fd2
Imran <imrandemadrid02@gmail.com>
2020-04-16 03:58:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas2/Main2Activity.java
@@ -0,0 +1,38 @@
+package com.example.tugas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenActivity3();
+            };
+        });
+
+
+    }
+
+    public void OpenActivity3(){
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/tugas2/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.tugas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView6);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this,"Register your account first :)",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
